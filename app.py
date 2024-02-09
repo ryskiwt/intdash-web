47e007f24420bb6cfe02d59d1330ffd086c1b289
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 08:38:42 +0000

    バグ修正

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ url = st.text_input("intdashサーバーURL", placeholder="https://example.com")
 token = st.text_input("APIトークン", type="password")
 node_id = st.text_input("ノードID")
 
-if st.button("接続する")
+if st.button("接続する"):
     resp = requests.get(
         url=f"{url}/api/auth/users/me",
         headers={"X-Intdash-Token": token},
