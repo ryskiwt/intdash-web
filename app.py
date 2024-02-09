1f9cd0cd521e5dc2ae322939c8877c1cf225dc2f
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 20:46:24 +0000

    更新

--- app.py
+++ app.py
@@ -9,9 +9,13 @@ def set_to_ls(k, v):
 
 st.title("認証情報")
 
-url = st.text_input(label="intdashサーバーURL", placeholder="https://example.com", value=get_from_ls("url"))
-token = st.text_input(label="APIトークン", type="password", value=get_from_ls("token"))
-project_uuid = st.text_input("プロジェクトID", value=get_from_ls("project_uuid"))
+st.write(get_from_ls("url"))
+st.write(get_from_ls("token"))
+st.write(get_from_ls("project_uuid"))
+
+url = st.text_input(label="intdashサーバーURL", placeholder="https://example.com")
+token = st.text_input(label="APIトークン", type="password")
+project_uuid = st.text_input("プロジェクトID")
 
 if st.button("保存する"):
     set_to_ls("url", url)
@@ -21,3 +25,14 @@ if st.button("保存する"):
     st.session_state["url"] = url
     st.session_state["token"] = token
     st.session_state["project_uuid"] = project_uuid
+
+    st.write("local storage")
+    st.write(get_from_ls("url"))
+    st.write(get_from_ls("token"))
+    st.write(get_from_ls("project_uuid"))
+
+    st.write("session state")
+    st.write(st.session_state["url"])
+    st.write(st.session_state["token"])
+    st.write(st.session_state["project_uuid"])
+
