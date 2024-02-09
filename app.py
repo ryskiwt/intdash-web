4314e506f542b0f9a17e4c1b5840f22c07617349
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 22:53:35 +0000

    更新

--- app.py
+++ app.py
@@ -42,5 +42,7 @@ if project_uuid:
     st.session_state.project_uuid = project_uuid
     ls_set("project_uuid", project_uuid)
 
-st.sidebar.header("認証情報", divider=True)
-st.sidebar.table([["URL", "プロジェクト", "トークン"], [url, project_uuid, None if token is None else "*****"]])
+st.sidebar.markdown("## 認証情報")
+st.sidebar.markdown(f"- **サーバーURL**: {url}")
+st.sidebar.markdown(f"- **プロジェクト**: {project_uuid}")
+st.sidebar.markdown(f"- **APIトークン**: { None if token is None else "*****"}")
