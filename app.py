bf9ba9fcd0b878f6d62a7abf68ff132bac7185fa
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 08:36:00 +0000

    最初のコミット

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import requests
+
+url = st.text_input("intdashサーバーURL", placeholder="https://example.com")
+token = st.text_input("APIトークン", type="password")
+node_id = st.text_input("ノードID")
+
+if st.button("接続する")
+    resp = requests.get(
+        url=f"{url}/api/auth/users/me",
+        headers={"X-Intdash-Token": token},
+    )
+    resp.raise_for_status()
+    st.write(resp.json())
