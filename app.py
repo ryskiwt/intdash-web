c54a57164baf184978aac1add7b894b765c7d4d3
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 21:36:58 +0000

    更新

--- app.py
+++ app.py
@@ -3,11 +3,25 @@ from streamlit_local_storage import LocalStorage
 
 ls = LocalStorage() 
 
+def ls_get(item_key, key):
+    if item_key is None:
+        return None
+
+    item = ls.getItem(key, key=key if key is None else f"get_item_{ietm_key}")
+    if item["storage"] is None:
+        return None
+    else:
+        return item["storage"]["value"]
+
+def ls_set(item_key, item_value):
+    ls.setItem(item_key, item_value)
+
+
 st.title("認証情報")
 
-url = ls.getItem("url", key="get_item_url")
-token = ls.getItem("token", key="get_item_token")
-project_uuid = ls.getItem("project_uuid", key="get_item_project_uuid")
+url_item = ls_get("url")
+token = ls_get("token")
+project_uuid = ls_get("project_uuid")
 
 st.write(f"url: {url}")
 st.write(f"token: {token}")
@@ -18,9 +32,9 @@ token = st.text_input(label="APIトークン", type="password", value=token)
 project_uuid = st.text_input("プロジェクトID", value=project_uuid)
 
 if st.button("保存する"):
-    ls.setItem("url", url)
-    ls.setItem("token", token)
-    ls.setItem("project_uuid", project_uuid)
+    ls_set("url", url)
+    ls_set("token", token)
+    ls_set("project_uuid", project_uuid)
 
     st.session_state["url"] = url
     st.session_state["token"] = token
