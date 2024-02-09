1994dca5b1946b15fd8a87206247b0ad331cb54e
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-09 23:30:15 +0000

    更新

--- app.py
+++ app.py
@@ -23,11 +23,8 @@ if ("token" not in st.session_state) or (st.session_state.token is None):
     st.session_state.token = ls_get("token")
 if ("project_uuid" not in st.session_state) or (st.session_state.project_uuid is None):
     st.session_state.project_uuid = ls_get("project_uuid")
-if ("project_name" not in st.session_state) or (st.session_state.project_uuid is None):
-    st.session_state.project_name = ls_get("project_name")
 
 st.header("認証情報", divider=True)
-
 url = st.text_input(label="intdashサーバーURL", placeholder="https://example.com", value=st.session_state.url)
 token = st.text_input(label="APIトークン", type="password", value=st.session_state.token)
 project_uuid = st.text_input(label="プロジェクトID", placeholder="00000000-0000-0000-0000-000000000000", value=st.session_state.project_uuid)
@@ -35,20 +32,29 @@ project_uuid = st.text_input(label="プロジェクトID", placeholder="00000000
 st.session_state.url = url
 st.session_state.token = token
 st.session_state.project_uuid = project_uuid
+
 ls_set("url", url)
 ls_set("token", token)
 ls_set("project_uuid", project_uuid)
 
 try:
     resp = requests.get(
-        url=f"{url}/api/auth/projects/{project_uuid}",
-        headers={"X-Intdash-Token": token},
+        url=f"{url}/api/auth/projects/{st.session_state.project_uuid}",
+        headers={"X-Intdash-Token": st.session_state.token},
     )
     resp.raise_for_status()
     project_name = resp.json()["name"]
 
     st.session_state.project_name = project_name
-    ls_set("project_name", project_name)
+
+    resp = requests.get(
+        url=f"{url}/api/auth/users/me",
+        headers={"X-Intdash-Token": st.session_state.token},
+    )
+    resp.raise_for_status()
+    user_display_name = resp.json()["nickname"]
+
+    st.session_state.user_display_name = user_display_name
 
 except Exception as e:
     st.error(f"入力に誤りがあります。: {e}")
@@ -56,5 +62,6 @@ except Exception as e:
 masked_token = None if st.session_state.token is None else "*****"
 st.sidebar.markdown("# 認証情報")
 st.sidebar.markdown(f"- **サーバーURL**: {st.session_state.url}")
-st.sidebar.markdown(f"- **APIトークン**: {masked_token}")
 st.sidebar.markdown(f"- **プロジェクト**: {st.session_state.project_name}")
+st.sidebar.markdown(f"- **APIトークン**: {masked_token}")
+st.sidebar.markdown(f"- **ユーザー**: {user_display_name}")
