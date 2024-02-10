08892c7efa9229e0706c8948f219d072e69d81fb
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 20:04:55 +0000

    更新

--- pages/measurement.py
+++ pages/measurement.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import requests
 
-st.write(st.query_params.to_dict())
+query_params = st.query_params.to_dict()
+meas_uuid = query_params["uuid"]
+
+params = {
+    "name": meas_uuid
+}
+resp = requests.get(
+    url=f"{st.session_state.url}/api/v1/projects/{st.session_state.project_uuid}/data_ids",
+    headers={"X-Intdash-Token": st.session_state.token},
+    params=params,
+)
+resp.raise_for_status()
+resp = resp.json()
+
+st.write(resp["items"])
