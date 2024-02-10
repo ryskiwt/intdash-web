91b34905dce3453e1665952da9c9bb6b99308964
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 00:10:26 +0000

    更新

--- pages/measurements.py
+++ pages/measurements.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import requests
-from datetime import time
-from datetime import datetime
+from datetime import time, datetime, timezone
 
 uuid = st.text_input("UUID")
 name = st.text_input("計測名")
-start_date = st.date_input("開始日時（日付）")
+start_date = st.date_input("開始日時（日付）").astimezone(timezone.utc)
 start_time = st.time_input("開始日時（時刻）", value=time(0, 0))
 start_frac = st.number_input("開始日時（小数点以下）", min_value=0, max_value=999999999)
-end_date = st.date_input("終了日時（日付）")
+end_date = st.date_input("終了日時（日付）").astimezone(timezone.utc)
 end_time = st.time_input("終了日時（時刻）", value=time(0, 0))
 end_frac = st.number_input("終了日時（小数点以下）", min_value=0, max_value=999999999)
 
 if st.button("検索する"):
-    start_rfc3339 = datetime.combine(start_date, start_time).strftime(f'%Y-%m-%dT%H:%M:%S.{start_frac}%z')
-    end_rfc3339 = datetime.combine(end_date, end_time).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac}%z')
+    start_rfc3339 = datetime.combine(start_date, start_time).strftime(f'%Y-%m-%dT%H:%M:%S.{start_frac:09}Z')
+    end_rfc3339 = datetime.combine(end_date, end_time).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z')
     st.write(start_rfc3339)
     st.write(end_rfc3339)
 
