cd658eb0892d26d626b7624199e564f1d0a8fb79
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 23:42:25 +0000

    更新

--- pages/01_measurements.py
+++ pages/01_measurements.py
@@ -190,9 +190,9 @@ def cropped_start_end(basetime, duration, tz):
 with st.expander("検索結果", expanded=True):
     st.write(f"{st.session_state.page} / {st.session_state.total_page} ページ")
     with st.container():
-        col1, col2, col3 = st.columns([1, 4, 1])
+        col1, col2, col3 = st.columns([1, 2, 1])
         col1.button("< 前のページ", on_click=on_click_prev)
-        st.session_state.page = col2.slider("ページ", min_value=2, max_value=st.session_state.total_page, value=st.session_state.page, on_change=search)
+        st.session_state.page = col2.slider("ページ", min_value=1, max_value=st.session_state.total_page, value=st.session_state.page, on_change=search)
         col3.button("次のページ >", on_click=on_click_next)
 
     for i, item in enumerate(st.session_state.measurements):
