e7c15b07dbea64693b62c276bda9764f9cb37985
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 19:19:42 +0000

    更新

--- pages/measurements.py
+++ pages/measurements.py
@@ -88,6 +88,7 @@ if search:
     if uuid is not None:
         params["uuid"] = uuid
     if edge_name_q is not None:
+        st.write(PATTERN.findall(edge_name_q))
         params["edge_uuid"] = PATTERN.findall(edge_name_q)
 
     resp = requests.get(
