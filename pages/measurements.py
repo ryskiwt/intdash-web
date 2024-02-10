09316b25de42929a7bc1ce498a5cf90944a3093b
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 19:44:53 +0000

    更新

--- pages/measurements.py
+++ pages/measurements.py
@@ -50,14 +50,15 @@ with st.expander("検索条件", expanded=True):
 
     with st.container():
         col1, col2 = st.columns(2)
-        name = col1.text_input("計測名")
-        uuid = col2.text_input("UUID")
+        name = col1.text_input("計測名", placeholder="Optional")
+        uuid = col2.text_input("UUID", placeholder="Optional")
     
     edge_name_q = st.selectbox("エッジ名", [{"name": v, "uuid": k} for k,v in EDGE_NAME_MAP.items()], format_func=lambda item: item["name"], index=None)
 
     tz = st.selectbox("タイムゾーン", pytz.common_timezones, index=pytz.common_timezones.index("Asia/Tokyo"))
-    page = st.number_input("ページ", value=1)
-    search = st.button("検索する")
+
+page = st.number_input("ページ", value=1, min_value=1)
+search = st.button("検索する")
 
 if search:
     limit = 50
