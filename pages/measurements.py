6b2ec967bbfc066644be64688895d79965a64104
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 00:22:10 +0000

    更新

--- pages/measurements.py
+++ pages/measurements.py
@@ -1,21 +1,40 @@
 import streamlit as st
 import requests
 from datetime import time, datetime, timezone
+from zoneinfo import ZoneInfo
 
 uuid = st.text_input("UUID")
 name = st.text_input("計測名")
-start_date = st.date_input("開始日時（日付）").astimezone(timezone.utc)
+timezone = st.text_input("タイムゾーン", "Asia/Tokyo")
+start_date = st.date_input("開始日時（日付）")
 start_time = st.time_input("開始日時（時刻）", value=time(0, 0))
 start_frac = st.number_input("開始日時（小数点以下）", min_value=0, max_value=999999999)
-end_date = st.date_input("終了日時（日付）").astimezone(timezone.utc)
+end_date = st.date_input("終了日時（日付）")
 end_time = st.time_input("終了日時（時刻）", value=time(0, 0))
 end_frac = st.number_input("終了日時（小数点以下）", min_value=0, max_value=999999999)
 
+start_rfc3339 = datetime.datetime(
+    start_date.year,
+    start_date.month,
+    start_date.day,
+    start_time.hour,
+    start_time.minute,
+    start_time.second,
+    timezone=ZoneInfo(timezone),
+).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z')
+end_rfc3339 = datetime.datetime(
+    end_date.year,
+    end_date.month,
+    end_date.day,
+    end_time.hour,
+    end_time.minute,
+    end_time.second,
+    timezone=ZoneInfo(timezone),
+).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z')
+st.write(start_rfc3339)
+st.write(end_rfc3339)
+
 if st.button("検索する"):
-    start_rfc3339 = datetime.combine(start_date, start_time).strftime(f'%Y-%m-%dT%H:%M:%S.{start_frac:09}Z')
-    end_rfc3339 = datetime.combine(end_date, end_time).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z')
-    st.write(start_rfc3339)
-    st.write(end_rfc3339)
 
     resp = requests.get(
         url=f"{url}/api/v1/projects/{st.session_state.project_uuid}/measurements",
