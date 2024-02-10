bc12b91b03181050185ab702e2bff9c6a8394604
Ryosuke Iwata <iw.06.ry.10@gmail.com>
2024-02-10 22:31:57 +0000

    更新

--- pages/01_measurements.py
+++ pages/01_measurements.py
@@ -35,126 +35,158 @@ while True:
         break
 EDGE_UUID_MAP = {v:k for k,v in EDGE_NAME_MAP.items()}
 
-with st.expander("検索条件", expanded=True):
-    with st.container():
-        col1, col2, col3 = st.columns(3)
-        start_date = col1.date_input("開始日時（日付）")
-        start_time = col2.time_input("開始日時（時刻）", value=time(0, 0))
-        start_frac = col3.number_input("開始日時（小数点以下）", min_value=0, max_value=999999999)
 
-    with st.container():
-        col1, col2, col3 = st.columns(3)
-        end_date = col1.date_input("終了日時（日付）")
-        end_time = col2.time_input("終了日時（時刻）", value=time(0, 0))
-        end_frac = col3.number_input("終了日時（小数点以下）", min_value=0, max_value=999999999)
+def declare_variable(name, default):
+    if name not in st.session_state:
+        st.session_state[name] = default
+
+declare_variable("measurements", [])
+declare_variable("total_page", 0)
+declare_variable("checked_measurement_uuids", [])
+
+
+def on_click_search(start_date, start_time, start_frac, end_date, end_time, end_frac, meas_name, meas_uuid, edge_uuid, page, limit):
+    limit = 50
+    params = {
+        "start": datetime(
+            start_date.year,
+            start_date.month,
+            start_date.day,
+            start_time.hour,
+            start_time.minute,
+            start_time.second,
+            tzinfo=ZoneInfo(tz),
+        ).astimezone(timezone.utc).strftime(f'%Y-%m-%dT%H:%M:%S.{start_frac:09}Z'),
+        "end": datetime(
+            end_date.year,
+            end_date.month,
+            end_date.day,
+            end_time.hour,
+            end_time.minute,
+            end_time.second,
+            tzinfo=ZoneInfo(tz),
+        ).astimezone(timezone.utc).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z'),
+        "page": page,
+        "limit": limit,
+    }
+    if name is not None:
+        params["name"] = meas_name
+    if uuid is not None:
+        params["uuid"] = meas_uuid
+    if edge_name_q is not None:
+        params["edge_uuid"] = edge_uuid
+
+    resp = requests.get(
+        url=f"{st.session_state.url}/api/v1/projects/{st.session_state.project_uuid}/measurements",
+        headers={"X-Intdash-Token": st.session_state.token},
+        params=params,
+    )
+    resp.raise_for_status()
+    resp = resp.json()
+
+    st.session_state.total_page = -((-resp["page"]["total_count"])//limit)
+    st.session_state.measurements = resp["items"]
+
+
+with st.expander("検索条件", expanded=True):
+    def craete_datetime_input(label):
+        with st.container():
+            col1, col2, col3 = st.columns(3)
+            start_date = col1.date_input(label=f"{label}（日付）")
+            start_time = col2.time_input(label=f"{label}（時刻）", value=time(0, 0))
+            start_frac = col3.number_input(label=f"{label}（小数点以下）", min_value=0, max_value=999999999)
+
+    craete_datetime_input(label="開始日時")
+    craete_datetime_input(label="終了日時")
 
     with st.container():
         col1, col2 = st.columns(2)
-        name = col1.text_input("計測名", placeholder="Optional")
-        uuid = col2.text_input("UUID", placeholder="Optional")
+        meas_name = col1.text_input(label="計測名", placeholder="Optional")
+        meas_uuid = col2.text_input(label="UUID", placeholder="Optional")
     
-    edge_name_q = st.selectbox("エッジ名", [{"name": v, "uuid": k} for k,v in EDGE_NAME_MAP.items()], format_func=lambda item: item["name"], index=None)
+    edge_name_q = st.selectbox(
+        label="エッジ名",
+        options=[{"name": v, "uuid": k} for k,v in EDGE_NAME_MAP.items()],
+        format_func=lambda item: item["name"],
+        index=None,
+    )
+    
+    tz = st.selectbox(
+        label="タイムゾーン",
+        options=pytz.common_timezones,
+        index=pytz.common_timezones.index("Asia/Tokyo"),
+    )
 
-    tz = st.selectbox("タイムゾーン", pytz.common_timezones, index=pytz.common_timezones.index("Asia/Tokyo"))
-    page = st.number_input("ページ", value=1, min_value=1)
-    search = st.button("検索する")
+    with st.container():
+        col1, col2 = st.columns(2)
+        page = col1.number_input("ページ", value=1, min_value=1)
+        limit = col2.number_input("件数/ページ", value=50, min_value=1)
 
-if ("checked_measurements" not in st.session_state) or (st.session_state.checked_measurements is None):
-    st.session_state.checked_measurements = set()
+    st.button("検索する", on_click=on_click_search, kwargs={
+        "start_date": start_date, 
+        "start_time": start_time, 
+        "start_frac": start_frac, 
+        "end_date": end_date, 
+        "end_time": end_time, 
+        "end_frac": end_frac, 
+        "meas_name": meas_name, 
+        "meas_uuid": meas_uuid, 
+        "edge_uuid": edge_name_q["uuid"], 
+        "page": page,
+        "limit": limit, 
+    })
 
 with st.expander("検索結果", expanded=True):
-    st.write("hoge")
-    if search:
-        limit = 50
-        params = {
-            "start": datetime(
-                start_date.year,
-                start_date.month,
-                start_date.day,
-                start_time.hour,
-                start_time.minute,
-                start_time.second,
-                tzinfo=ZoneInfo(tz),
-            ).astimezone(timezone.utc).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z'),
-            "end": datetime(
-                end_date.year,
-                end_date.month,
-                end_date.day,
-                end_time.hour,
-                end_time.minute,
-                end_time.second,
-                tzinfo=ZoneInfo(tz),
-            ).astimezone(timezone.utc).strftime(f'%Y-%m-%dT%H:%M:%S.{end_frac:09}Z'),
-            "page": page,
-            "limit": limit,
-        }
-        if name is not None:
-            params["name"] = name
-        if uuid is not None:
-            params["uuid"] = uuid
-        if edge_name_q is not None:
-            params["edge_uuid"] = edge_name_q["uuid"]
+    st.write(f"{page} / {st.session_state.total_page} pages")
 
-        resp = requests.get(
-            url=f"{st.session_state.url}/api/v1/projects/{st.session_state.project_uuid}/measurements",
-            headers={"X-Intdash-Token": st.session_state.token},
-            params=params,
-        )
-        resp.raise_for_status()
-        resp = resp.json()
+    for i, item in enumerate(st.session_state.measurements):
+        with st.container(border=True):
+            meas_name = "<名称なし>" if item["name"]=="" else item["name"]
+            meas_uuid = item["uuid"]
+            duration = timedelta(microseconds=item["max_elapsed_time"])
 
-        total_page = -((-resp["page"]["total_count"])//limit)
-        st.write(f"{page} / {total_page} pages")
+            splitted = item["basetime"].split(".")
+            start_time = datetime.fromisoformat(splitted[0]).astimezone(ZoneInfo(tz))
+            end_time = (start_time + duration)
 
-        for i, item in enumerate(resp["items"]):
-            with st.container(border=True):
-                meas_name = item["name"]
-                meas_name = "-- 名称なし --" if meas_name=="" else meas_name
-                meas_uuid = item["uuid"]
-                duration = timedelta(microseconds=item["max_elapsed_time"])
+            hours = duration.seconds // 3600
+            minutes = (duration.seconds - hours*3600) // 60
+            seconds = duration.seconds - hours*3600 - minutes*60
+            hours += duration.days*24
 
-                splitted = item["basetime"].split(".")
-                start_time = datetime.fromisoformat(splitted[0]).astimezone(ZoneInfo(tz))
-                end_time = (start_time + duration)
+            edge_uuid = item["edge_uuid"]
+            edge_name = EDGE_NAME_MAP[edge_uuid]
 
-                hours = duration.seconds // 3600
-                minutes = (duration.seconds - hours*3600) // 60
-                seconds = duration.seconds - hours*3600 - minutes*60
-                hours += duration.days*24
+            with st.container():
+                col1, col2 = st.columns(2)
+                col1.write(start_time.strftime("%Y/%m/%d %H:%M:%S") + " - " + end_time.strftime("%Y/%m/%d %H:%M:%S"))
+                col2.write(f"{hours} 時間 {minutes} 分 {seconds} 秒")
 
-                edge_uuid = item["edge_uuid"]
-                edge_name = EDGE_NAME_MAP[edge_uuid]
+            with st.container():
+                col1, col2 = st.columns(2)
+                col1.write("ステータス: " + STATUS_MAP[item["sequences"]["status"]])
 
-                with st.container():
-                    col1, col2 = st.columns(2)
-                    col1.write(start_time.strftime("%Y/%m/%d %H:%M:%S") + " - " + end_time.strftime("%Y/%m/%d %H:%M:%S"))
-                    col2.write(f"{hours} 時間 {minutes} 分 {seconds} 秒")
+                received_chunks_ratio = item["sequences"]["received_chunks_ratio"] * 100.0
+                received_data_points = item["sequences"]["received_data_points"]
+                expected_data_points = item["sequences"]["expected_data_points"]
+                processed_ratio = item["processed_ratio"] * 100.0
 
-                with st.container():
-                    col1, col2 = st.columns(2)
-                    col1.write("ステータス: " + STATUS_MAP[item["sequences"]["status"]])
-
-                    received_chunks_ratio = item["sequences"]["received_chunks_ratio"] * 100.0
-                    received_data_points = item["sequences"]["received_data_points"]
-                    expected_data_points = item["sequences"]["expected_data_points"]
-                    processed_ratio = item["processed_ratio"] * 100.0
-
-                    if received_data_points <= expected_data_points:
-                        col2.write(f"{received_chunks_ratio:3.1f} % ({received_data_points} / {expected_data_points} points)")
-                    else:
-                        col2.write(f"{processed_ratio:3.1f} % (iSCPv1)")
-
-                st.write(f"エッジ名: [{edge_name}]({st.session_state.url}/console/edges/{edge_uuid}/?projectUuid={st.session_state.project_uuid})  ({edge_uuid})")
-                st.write(f"計測名: [{meas_name}]({st.session_state.url}/console/measurements/{meas_uuid}/?projectUuid={st.session_state.project_uuid}) ({meas_uuid})")
-
-                checked = meas_uuid in st.session_state.checked_measurements
-                if st.checkbox("この計測を対象にする", key=f"checkbox_{i}", value=checked):
-                    st.session_state.checked_measurements.add(meas_uuid)
+                if received_data_points <= expected_data_points:
+                    col2.write(f"{received_chunks_ratio:3.1f} % ({received_data_points} / {expected_data_points} points)")
                 else:
-                    if checked:
-                        st.session_state.checked_measurements.remove(meas_uuid)
+                    col2.write(f"{processed_ratio:3.1f} % (iSCPv1)")
 
-st.write(st.session_state.checked_measurements)
+            st.write(f"エッジ名: [{edge_name}]({st.session_state.url}/console/edges/{edge_uuid}/?projectUuid={st.session_state.project_uuid})  ({edge_uuid})")
+            st.write(f"計測名: [{meas_name}]({st.session_state.url}/console/measurements/{meas_uuid}/?projectUuid={st.session_state.project_uuid}) ({meas_uuid})")
+
+            checked = meas_uuid in st.session_state.checked_measurement_uuids
+            if st.checkbox("この計測を対象にする", key=f"checkbox_{i}", value=checked):
+                st.session_state.checked_measurement_uuids.add(meas_uuid)
+            else:
+                if checked:
+                    st.session_state.checked_measurement_uuids.remove(meas_uuid)
+
+st.write(st.session_state.checked_measurement_uuids)
 
 
 
